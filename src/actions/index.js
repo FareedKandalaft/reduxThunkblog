26b1e2886e00376e6761d83367b0ca915f2bf34e
FareedKandalaft <fareed@spectrecoder.com>
2021-08-08 22:23:52 -0400

    Set up for axios calls and asynchronous Action Creator for fetchposts.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
-export const fetchPosts = () => {
-  return {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async (dispatch) => {
+  const resp = await jsonPlaceholder.get('/posts');
+
+  dispatch({
     type: 'FETCH_POSTS',
-    payload: null,
-  };
+    payload: resp.data,
+  });
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
-  proxyReducer: () => 'Proxy Reducer',
+  posts: postsReducer,
 });
